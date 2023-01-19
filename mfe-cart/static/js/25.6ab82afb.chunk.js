b37bd596e982a12a1d31d12292fb4bcf64cb4785
Przemyslaw Szopa <prz.szo@gmail.com>
2023-01-19 20:22:53 +0100

    Updates

---
+++ mfe-cart/static/js/25.6ab82afb.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkcart=self.webpackChunkcart||[]).push([[25],{66025:function(e,n,a){Promise.all([a.e(710),a.e(993)]).then(a.bind(a,48993))}}]);
+//# sourceMappingURL=25.6ab82afb.chunk.js.map
